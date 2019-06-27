9a6ebcd0045ca1744724c378f5959530e578730c
aleks989 <47993376+aleks989@users.noreply.github.com>
2019-06-27 14:57:58 +0200

    Serbian Translation
    Serbian translation for backend

---
+++ resources/lang/sr/response.php
@@ -0,0 +1,29 @@
+<?php
+
+// Serbian translation
+return [
+    'noConfig'     => 'Nema konfiguracije!',
+    'notFound'     => 'Nije pronađeno!',
+    'diskNotFound' => 'Disk nije pronađen!',
+    'pathNotFound' => 'Putanja nije pronađena!',
+    'diskSelected' => 'Disk izabran!',
+    // files
+    'fileExist'    => 'Datoteka već postoji!',
+    'fileCreated'  => 'Nova datoteka napravljena!',
+    'fileUpdated'  => 'Datoteka izmenjena!',
+    'fileNotFound' => 'Datoteka nije pronadjena!',
+    // directories
+    'dirExist'     => 'Direktorijum već postoji!',
+    'dirCreated'   => 'Novi direktorijum napravljen!',
+    'dirNotFound'  => 'Direktorijum nije pronađen',
+    // actions
+    'uploaded'     => 'Sve datoteke poslate!',
+    'delNotFound'  => 'Nekoliko stavki nije pronađeno! Osvežite!',
+    'deleted'      => 'Obrisano!',
+    'renamed'      => 'Preimenovano!',
+    'copied'       => 'Uspešno kopirano!',
+    // zip
+    'zipError'     => 'Greška u pravljenju arhive!',
+    // acl
+    'aclError'     => 'Pristup odbijen!',
+];
