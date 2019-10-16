8a1d602fccf2d7ac79ab9a4caf145d58d29fb153
Laurens <99005216@mydavinci.nl>
2019-10-16 14:05:28 +0200

    Translated into Dutch

---
+++ resources/lang/nl/response.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'noConfig'       => 'Config niet gevonden!',
+    'notFound'       => 'Niet gevonden!',
+    'diskNotFound'   => 'Schijf niet gevonden!',
+    'pathNotFound'   => 'Pad niet gevonden!',
+    'diskSelected'   => 'Schijf geselecteerd!',
+    // files
+    'fileExist'      => 'Bestand bestaat al!',
+    'fileCreated'    => 'Bestand aangemaakt!',
+    'fileUpdated'    => 'Bestand bijgewerkt!',
+    'fileNotFound'   => 'Bestand niet gevonden!',
+    // directories
+    'dirExist'       => 'Folder bestaat al!',
+    'dirCreated'     => 'Folder aangemaakt!',
+    'dirNotFound'    => 'Folder niet gevonden',
+    // actions
+    'uploaded'       => 'Alle bestanden geüpload!',
+    'notAllUploaded' => 'Sommige bestanden zijn niet geüpload!',
+    'delNotFound'    => 'Sommige bestanden konden niet worden gevonden!',
+    'deleted'        => 'Verwijderd!',
+    'renamed'        => 'Hernoemd!',
+    'copied'         => 'Succesvol gekopieerd!',
+    // zip
+    'zipError'       => 'Fout bij het maken van archief!',
+    // acl
+    'aclError'       => 'Toegang geweigerd!',
+];
